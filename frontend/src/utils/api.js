f6452ad5e594bfd2381507e3737a546afa97ced4
Lakshit Bhansali <101925867+Lakshitb1@users.noreply.github.com>
2024-11-23 16:27:11 +0530

    Update api.js

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API = axios.create({ baseURL: "http://file-morpher.vercel.app" });
+const API = axios.create({ baseURL: "https://file-morpher.vercel.app" });
 
 export const convertFile = (formData) =>
   API.post("/convertFile", formData, {
