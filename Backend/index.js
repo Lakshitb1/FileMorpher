4811bd7d31cf4f2fb08db73b8bdcd055fe612998
Lakshit Bhansali <101925867+Lakshitb1@users.noreply.github.com>
2024-11-23 16:12:05 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000;
 
 app.use(cors(
   {
-    origin: ["https://file-morpher-frontend.vercel.app/"],
+    origin: ["https://file-morpher-frontend.vercel.app"],
   }
 ));
 app.use(express.json());
