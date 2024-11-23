47f6dd538c19c8d971f75749ca30055b4c8d84c3
Lakshit Bhansali <101925867+Lakshitb1@users.noreply.github.com>
2024-11-23 16:14:52 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -7,7 +7,7 @@ const fs = require("fs");
 const { PDFDocument } = require("pdf-lib");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const cors = require('cors');
 app.use(cors({
