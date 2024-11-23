bba2668f3c42d0e3981ece1f5c9d8995db709bb3
Lakshit Bhansali <101925867+Lakshitb1@users.noreply.github.com>
2024-11-23 16:54:13 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -1,21 +1,15 @@
 const express = require("express");
 const multer = require("multer");
+const cors = require("cors");
 const docxToPDF = require("docx-pdf");
 const path = require("path");
 const fs = require("fs");
 const { PDFDocument } = require("pdf-lib");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
-const cors = require('cors');
-app.use(cors({
-  origin: [
-    "https://file-morpher-frontend.vercel.app", // Production URL
-    "http://localhost:3000" // Local development URL
-  ]
-}));
-
+app.use(cors());
 app.use(express.json());
 
 // Ensure uploads directory exists
